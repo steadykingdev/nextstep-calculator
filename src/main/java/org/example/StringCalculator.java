c12f94e14c3f521d738746887995f5add7ce45ae
steadykingdev <steadykingdev@gmail.com>
2023-04-21 15:25:34 +0900

    refactor: contain(,) 필요 없어서 if문 제거

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -9,15 +9,12 @@ public class StringCalculator {
             return 0;
         }
 
-        if (text.contains(",")) {
-            String[] values = text.split(",");
-            int sum = 0;
-            for (String value : values) {
-                sum += Integer.parseInt(value);
-            }
-            return sum;
+        String[] values = text.split(",");
+        int sum = 0;
+        for (String value : values) {
+            sum += Integer.parseInt(value);
         }
 
-        return Integer.parseInt(text);
+        return sum;
     }
 }
