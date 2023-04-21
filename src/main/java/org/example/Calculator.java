41e5cc2afde82f93f1d43d32fab6d1125d6f41dd
steadykingdev <steadykingdev@gmail.com>
2023-04-21 11:32:52 +0900

    refactor: test코드 분리

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -16,12 +16,4 @@ public class Calculator {
     int divide(int i, int j) {
         return i / j;
     }
-
-    public static void main(String[] args) {
-        Calculator cal = new Calculator();
-        System.out.println(cal.add(3, 4));
-        System.out.println(cal.subtract(5, 4));
-        System.out.println(cal.multiply(2, 6));
-        System.out.println(cal.divide(8, 4));
-    }
 }
